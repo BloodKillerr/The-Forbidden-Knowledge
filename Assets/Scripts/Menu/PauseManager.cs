b20f576de51919bc1c78e223ff0e7a8dd1baea68
xGlobii <kapitanglobii@gmail.com>
2025-05-10 18:40:12 +0200

    added a pause menu and a menu for notes

---
+++ Assets/Scripts/Menu/PauseManager.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+	public static PauseManager Instance { get; private set; }
+
+	[SerializeField] private GameObject pausePanel;
+	[SerializeField] private GameObject optionsPanel;
+	[SerializeField] private GameObject notesPanel;
+
+	private void Awake()
+	{
+		if (Instance == null)
+			Instance = this;
+		else
+		{
+			Destroy(gameObject);
+			return;
+		}
+	}
+
+	public void Resume()
+	{
+		Debug.Log("Resume");
+	}
+
+	public void Save()
+	{
+		Debug.Log("Save");
+	}
+
+	public void Notes()
+	{
+		if (notesPanel.activeInHierarchy)
+		{
+			notesPanel.SetActive(false);
+		}
+		else
+		{
+			notesPanel.SetActive(true);
+			NotesManager.Instance.PopulateNotesList();
+		}
+	}
+
+	public void Options()
+	{
+		if (optionsPanel.activeInHierarchy)
+		{
+			optionsPanel.SetActive(false);
+			pausePanel.SetActive(true);
+		}
+		else
+		{
+			optionsPanel.SetActive(true);
+			pausePanel.SetActive(false);
+		}
+	}
+
+	public void MainMenu()
+	{
+		Debug.Log("Main Menu");
+	}
+
+	public void Quit()
+	{
+		Debug.Log("Quit");
+		Application.Quit();
+	}
+}
